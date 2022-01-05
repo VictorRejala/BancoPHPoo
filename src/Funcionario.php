b8506f5161874442ab1ff30b3a617eeca32e93ee
Rejala <victorrejala@gmail.com>
2022-01-05 03:20:54 -0300

    Herença

--- src/Funcionario.php
+++ src/Funcionario.php
@@ -1,31 +1,24 @@
 <?php
 
-class Funcionario
+class Funcionario extends Pessoa
 {
 
-    private $nome;
-    private $cpf;
     private $cargo;
 
-    public function __construct(string $nome,string $cpf, string $cargo)
+    public function __construct(string $nome,CPF $cpf, string $cargo)
     {
-        $this->nome = $nome;
-        $this->cpf = $cpf;
+        parent::__construct($nome, $cpf);
         $this->cargo = $cargo;
-    }
-
-    public function recuperaNome(): string
-    {
-        return $this->nome;
-    }
-
-    public function recuperaCpf(): string
-    {
-        return $this->cpf;
     }
 
     public function recuperaCargo(): string
     {
         return $this->cargo;
+    }
+
+    public function alteraNome(string $nome):void
+    {
+        $this->validaNomeTitular($nome);
+        $this->nome = $nome;
     }
 }
